0f502e0e96ade9c81dbc693616def32040a02f8f
0EFB6 <changws12345@gmail.com>
2021-09-03 16:25:56 +0800

    new file:   keybinds.sh

---
+++ keybinds.sh
@@ -0,0 +1,51 @@
+echo "DWM Keybins
+M-T				: Tiling mode
+M-F				: Floating mode
+M-M				: Stacking mode
+M-J, M-Grave	: Focus next window
+M-K				: Focus previous window
+M-G				: Decrease window size 
+M-H				: Increase window size
+M-Ctrl-Space	: Focus first window
+M-Tab			: Switch to previous workspace
+M-C				: Closs focused window
+M-B				: Toggle dwm bar
+M-Space			: Change window mode
+
+M-(1-9)			: Go to workspace 1-9
+M-Shift-(1-9)	: Move selected window to workspace 1-9
+
+M-Shift-Q		: Exit dwm
+
+System
+M-Shift-U		: Poweroff
+M-Shift-R		: Reboot
+
+
+Function Key
+F3				: Focus next window
+F4				: Focus previous window
+F6				: Lock
+
+Run prompt
+M-R				: Run a program
+M-A				: Show opened window
+
+Keybinds for launching program
+M-Enter			: Launch terminal
+M-W				: Launch Firefox
+M-E				: Launch Pcmanfm
+M-Y				: Launch Stui
+M-Z				: Launch Brave
+M-S				: Launch Spotify
+M-Q				: Launch Pulsemixer
+M-O				: Launch OBS
+
+Camera
+M-X				: Start school camera
+M-Shift-X		: Switch off school camera
+
+Screenshot
+PrintScreen		: Capture full screen
+M-PrintScreen	: Capture selected area
+"
